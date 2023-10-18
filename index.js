2114ceb910213233bd00ac4a7d3b3ae0024924ca
Shankar-94 <121161887+Shankar-94@users.noreply.github.com>
2023-10-18 23:42:38 +0530

    Paper-Library-Installed

--- index.js
+++ index.js
@@ -6,5 +6,23 @@ import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
 import 'react-native-gesture-handler';
+import { MD3LightTheme as DefaultTheme, PaperProvider } from 'react-native-paper';
 
-AppRegistry.registerComponent(appName, () => App);
+const theme = {
+    ...DefaultTheme,
+    colors: {
+      ...DefaultTheme.colors,
+      primary: 'tomato',
+      secondary: 'yellow',
+    },
+  }
+
+export default function Main() {
+    return (
+      <PaperProvider>
+        <App />
+      </PaperProvider>
+    );
+  }
+  
+  AppRegistry.registerComponent(appName, () => Main);
